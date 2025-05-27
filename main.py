d69e4fe1c0a22ff7980fc2bd455bdc0b7a7e915e
afrin-k <afrinkhanam318@gmail.com>
2025-05-27 20:10:59 +0530

    track highscore

--- main.py
+++ main.py
@@ -37,14 +37,14 @@ while game_is_on:
 
     # collision with wall
     if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()>280 or snake.head.ycor()<-280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # collision with tail
     if len(snake.segments) > 3: 
         for segment in snake.segments[1:]:
             if snake.head.distance(segment) < 10:
-                game_is_on = False
-                scoreboard.game_over()
+                scoreboard.reset()
+                snake.reset()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+import os
+
 ALIGN = "center"
 FONT = ("Trebuchet MS",20,"bold")
 
@@ -6,23 +8,29 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.f_path = os.path.join(os.path.dirname(__file__),"data.txt")
+        with open(self.f_path) as data:
+            self.highscore = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0,260)
         self.hideturtle()
         self.update_scoreboard()
-
+        
     # score manipulations
     def update_scoreboard(self):
-        self.write(f"Score = {self.score}",align=ALIGN,font=FONT)
+        self.clear()
+        self.write(f"Score = {self.score} | High Score = {self.highscore}",align=ALIGN,font=FONT)
 
     def increase_score(self):
         self.score += 1 
-        self.clear()
         self.update_scoreboard()
 
     # game over message
-    def game_over(self):
-        self.clear()
-        self.write(f"Game Over.",align=ALIGN,font=FONT)
-        self.goto(0,0)
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open(self.f_path,mode="w") as data:
+                data.write(str(self.highscore))
+        self.score = 0
+        self.update_scoreboard()
